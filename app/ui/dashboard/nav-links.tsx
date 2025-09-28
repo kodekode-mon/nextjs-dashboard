f49e2e1c4c334aa477c87fc645f7267edc8613d4
Jerald <jeraldngo959@gmail.com>
2025-09-28 15:56:36 +0800

    Feature added

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -37,6 +37,8 @@ export default function NavLinks() {
               },
             )}
           >
+
+            {/* comment */}
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
